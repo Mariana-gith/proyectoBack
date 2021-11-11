d8cc729cf176ac2b3d43a8d9ddd97c361cf2295e
Mariana-gith <mariana.a.iamarino@gmail.com>
2021-11-10 23:56:39 -0300

    websocket

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const app = express()
+const reouters = require("./routes/index")
+const http = require('http')
+const server = http.createServer(app)
+const io = require('socket.io')(server) 
+
+const port = 3001
+
+
+let msj= [] 
+
+
+//configuracion
+
+app.use(express.static(__dirname+ "/public"))
+
+
+// websocket
+
+io.on("connection",(socket)=>{
+
+    console.log('cliente conectado')
+
+    socket.on("menssege_back", (data)=>{
+        console.log(data)
+    })
+
+    socket.on("msn__client",(data)=>{
+        console.log(data)
+        msj.push(data)
+        console.log(msj)
+        socket.emit("menssenge_client", msj)
+    })
+
+});
+
+//Router
+
+app.use("/api", reouters)
+
+
+
+server.listen(port,()=>{
+    console.log('server ok!!',port)
+})
---
+++ public/index.js
@@ -0,0 +1,28 @@
+const socket= io()
+
+socket.on("menssenge_client",(data)=>{
+    console.log(data)
+    render(data)
+    socket.emit("menssege_back", "Mensaje recibido")
+})
+
+const render = (d) =>{
+    let html = d.map((da)=>{
+        return `<p> ${ da.usuario} :  ${ da.mensaje}  </p>`
+    }).join(" ")
+
+    document.querySelector("#caja").innerHTML = html
+}
+
+const addMessage = () =>{
+    let objMsj ={
+        usuario: document.getElementById('user').value,
+        mensaje: document.getElementById('msn').value
+    }
+
+    console.log(objMsj)
+    socket.emit("msn__client", objMsj)
+    mensaje: document.getElementById('msn').value= " "
+    usuario: document.getElementById('user').value= " "
+    return false
+}
